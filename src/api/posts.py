ff53af987bd7a9e93a09c8d4fcef6197789e9a11
Robert Syvarth <robert.syvarth@gmail.com>
2018-09-11 21:41:50 -0700

    Setup serverless framework

---
+++ src/api/posts.py
@@ -0,0 +1,32 @@
+import datetime
+import json
+
+def handler(event, context):
+    # Someday....
+    # posts = coreService.getPosts()
+
+    body = {
+        "posts": [
+            {
+                "id": 2,
+                "title": "Post about something",
+                "body": "This is my body content",
+                "created_at": datetime.datetime.now().isoformat(),
+                "updated_at": datetime.datetime.now().isoformat(),
+                "created_by": "Emily"
+            },
+            {
+                "id": 1,
+                "title": "Post about another thing",
+                "body": "Hello World",
+                "created_at": datetime.datetime.now().isoformat(),
+                "updated_at": datetime.datetime.now().isoformat(),
+                "created_by": "Robert"
+            }
+        ]
+    }
+
+    return {
+        "statusCode": 200,
+        "body": json.dumps(body)
+    }
