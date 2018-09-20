2b64afd7a2e16af34a5a7b1fd6e829545bbeec30
em-shea <c.emilyshea@gmail.com>
2018-09-20 16:12:40 -0700

    Create env_var.py

---
+++ src/api/env_var.py
@@ -0,0 +1,12 @@
+def lambda_handler(event, context):
+    # TODO implement
+    env_var_dict = {
+        'time': os.environ['TZ'],
+        'runtime': os.environ['AWS_EXECUTION_ENV'],
+        'lang': os.environ['LANG']
+    }
+    
+    return {
+        "statusCode": 200,
+        "body": json.dumps(env_var_dict)
+    }
