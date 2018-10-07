8187778795dfcc923c2a6a886266699b9e0be385
em-shea <c.emilyshea@gmail.com>
2018-10-07 16:05:11 -0700

    Clean up listing.py

--- src/core/posts/listing.py
+++ src/core/posts/listing.py
@@ -1,42 +1,21 @@
 import datetime
 import json
+import boto3
+client = boto3.client('s3')
 
+# Handler loops through files in the s3 posts bucket. It collects Bucket Name and Key data in order to read the post body. It then converts the post body to JSON and appends the JSON dictionary to a running list of post contents.
 def handler(event, context):
-    # TODO implement
-    import boto3
-    client = boto3.client('s3')
-
     s3 = boto3.resource('s3')
     bucket = s3.Bucket('tsu-posts')
+
     contents_dict_list = []
     for post_summary in bucket.objects.all():
         post_bucket_name = post_summary.bucket_name
         post_key = post_summary.key
         post = client.get_object(Bucket=post_bucket_name,Key=post_key)
+
         contents = post['Body'].read()
         contents_dict = json.loads(contents)
         contents_dict_list.append(contents_dict)
-        print(contents_dict)
+        
     return contents_dict_list
-
-    # Someday....
-    # posts = s3.readPosts()
-
-    # return [
-    #     {
-    #         "id": 2,
-    #         "title": "Post about something",
-    #         "body": "This is my body content",
-    #         "created_at": datetime.datetime.now().isoformat(),
-    #         "updated_at": datetime.datetime.now().isoformat(),
-    #         "created_by": "Emily"
-    #     },
-    #     {
-    #         "id": 1,
-    #         "title": "Post about another thing",
-    #         "body": "你好世界",
-    #         "created_at": datetime.datetime.now().isoformat(),
-    #         "updated_at": datetime.datetime.now().isoformat(),
-    #         "created_by": "Robert"
-    #     }
-    #]
