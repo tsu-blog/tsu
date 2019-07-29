33474b0f7d70b1367d9a6b9d041dbc451d85c2b1
Robert Syvarth <robert.syvarth@gmail.com>
2019-07-28 19:57:53 -0700

    Update post publishing task to upload local images

--- src/cli/commands/publish.py
+++ src/cli/commands/publish.py
@@ -4,6 +4,11 @@ import json
 import boto3
 from io import BytesIO
 import yaml
+import re
+import os
+
+from PIL import Image
+from resizeimage import resizeimage
 
 from commands.base import TsuCommand
 
@@ -15,10 +20,18 @@ class PublishCmd(TsuCommand):
         parser.add_argument('path', help="File path of new post to be uploaded")
 
     def run(self, args):
-        print("Formatting post...")
+        post_dir = os.path.dirname(os.path.realpath(args.path))
+
+        s3 = boto3.resource('s3')
+        cdn_bucket = s3.Bucket(self.get_cdn_bucket(args.stage))
+
         with open(args.path, 'r', encoding='utf-8') as fh:
             contents = fh.read()
 
+        print("Uploading images...")
+        contents = self.replace_images(contents, post_dir, cdn_bucket)
+
+        print("Formatting post...")
         html = markdown2.markdown(contents, extras=['metadata','fenced-code-blocks','footnotes','header-ids','tables'])
         data = {
             **html.metadata,
@@ -28,11 +41,67 @@ class PublishCmd(TsuCommand):
 
         # Upload the post to S3
         print("Uploading post to S3...")
-        s3 = boto3.resource('s3')
-        bucket = s3.Bucket(self.get_bucket(args.stage))
+        bucket = s3.Bucket(self.get_posts_bucket(args.stage))
         bucket.upload_fileobj(BytesIO(json.dumps(data).encode('utf-8')), f"{data['id']}.json")
 
-    def get_bucket(self, stage):
+    def replace_images(self, markdown, post_dir, cdn_bucket):
+        # Finds all matches for ![{alt}](./{img})({css})
+        for alt, image, maxwidth in re.findall('\!\[(.*?)\]\((\.\/.+?)\)(?:\(maxwidth\=(.+?)\))?', markdown):
+            images = self.upload_image_to_s3(image, post_dir, cdn_bucket)
+            img_full = None
+            srcset = []
+            for image_path, size in images:
+                if size is None:
+                    img_full = image_path
+                else:
+                    srcset.append(f'{image_path} {size}w')
+            srcset = ', '.join(srcset)
+
+            maxwidth_val = maxwidth if len(maxwidth) > 0 else '100%'
+            image_tag = f'<p style="text-align: center"><a href="{img_full}"><img src="{img_full}" alt="{alt}" srcset="{srcset}" sizes="(max-width: 480px) 100%, (max-width: 900px) 80vw, {maxwidth_val}"/></a></p>'
+            if len(maxwidth) > 0:
+                markdown = markdown.replace(f'![{alt}]({image})(maxwidth={maxwidth})', image_tag)
+            else:
+                markdown = markdown.replace(f'![{alt}]({image})', image_tag)
+
+        return markdown
+
+    def upload_image_to_s3(self, image_path, relative_dir, cdn_bucket, sizes=[None,320,640,1280]):
+        images = []
+        image_path = image_path.replace('./', '')
+        
+        with open(os.path.join(relative_dir, image_path), 'r+b') as f:
+            with Image.open(f) as image:
+                for size in sizes:
+                    # If size is None then we are keeping the original resolution
+                    image_sized_path = os.path.join('static','images', image_path)
+                    image_sized = image
+
+                    if size is not None:
+                        image_sized = resizeimage.resize_width(image, size)
+                        parts = image_sized_path.split('.')
+                        parts[-2] = f'{parts[-2]}-{size}'
+                        image_sized_path = '.'.join(parts)
+
+                    print(">", image_sized_path)
+
+                    # Save the image to a byte array and upload to S3
+                    imgByteArr = BytesIO()
+                    image_sized.save(imgByteArr, image.format)
+                    imgByteArr.seek(0)
+
+                    cdn_bucket.upload_fileobj(imgByteArr, image_sized_path, ExtraArgs={'ACL':'public-read', 'ContentType': Image.MIME[image_sized.format]})
+                    images.append((f'/{image_sized_path}', size))
+
+        return images
+
+    def get_posts_bucket(self, stage):
+        return f"{self.get_bucket_base(stage)}-posts"
+
+    def get_cdn_bucket(self, stage):
+        return f"{self.get_bucket_base(stage)}-static"
+
+    def get_bucket_base(self, stage):
         with open(self.path("config.yml"), 'r') as fh:
             data = yaml.safe_load(fh)
 
@@ -42,4 +111,4 @@ class PublishCmd(TsuCommand):
             config = data['default']
             stage = 'dev'
 
-        return f"tsu-{config['domain']}-{stage}-posts"
+        return f"tsu-{config['domain']}-{stage}"
