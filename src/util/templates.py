10f51266dbe5a451d1e38ed4d1a8e9c583e66a13
Robert Syvarth <robert.syvarth@gmail.com>
2019-09-02 21:22:07 -0700

    Restructure custom content

--- src/util/templates.py
+++ src/util/templates.py
@@ -5,10 +5,15 @@ from jinja2 import Environment
 from jinja2 import FileSystemLoader
 
 def render(template, data):
-    # Load the template
+    # Pull the path to our /custom/templates directory
+    custom_template_dir = os.path.join(os.path.abspath(os.path.dirname(__file__)), '..',  '..', 'custom', 'templates')
+
+    # Pull the path to the configured built-in template
     templates_dir = os.path.join(os.path.abspath(os.path.dirname(__file__)), '..',  'ui', 'templates')
-    active_template_dir = os.path.join(templates_dir, ConfigValues.TEMPLATE)
-    env = Environment(loader=FileSystemLoader(active_template_dir))
+    base_template_dir = os.path.join(templates_dir, ConfigValues.TEMPLATE)
+
+    # Load the template
+    env = Environment(loader=FileSystemLoader([custom_template_dir, base_template_dir]))
     template = env.get_template(template)
 
     # Add in some default keys to our data
